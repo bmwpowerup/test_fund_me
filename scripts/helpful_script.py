de469957e55127f063443964c9d7aa104a82ae2d
bmwpowerup <zhiyi@Zhiyis-Mac-mini.local>
2022-05-16 12:07:50 +0800

    first comit

---
+++ scripts/helpful_script.py
@@ -0,0 +1,11 @@
+from brownie import network, config, accounts, MockV3Aggregator
+from web3 import 
+
+def get_account():
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
